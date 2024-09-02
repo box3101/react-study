7d6dfb1575c8ead6d71ea2afa2fe760083d0d7d1
lcy <lcy@naver.com>
2024-09-02 23:42:41 +0900

    멤버 데이터를 state에 담아서 화면 출력

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,28 +1,25 @@
 import Layout from "../components/Layout";
 import { useState } from "react";
 
-//리액트의 컴포넌트 단위의 개발이 효율적인 이유
-//1. 기능별로 개발로직을 따로 관리 (로그인(유저정보), 주메뉴(메뉴정보), 게시판(게시글정보), 썸네일(이미지정보) )
-//2. 데이터를 기반으로 데이터의 변경을 인지해서 리액트 자체적으로 자동 렌더링 (변경점 화면에 갱신)
-//3. state라는 걸로 데이터를 관리, state전용 변경함수로만 데이터 변경가능 (중요한 데이터를 쉽게 변경치 않도록 강제)
-
 function Members() {
-	console.log("member render");
-	const [Num, setNum] = useState(0);
+	const [Members, setMembers] = useState([
+		{ name: "David", position: "President", pic: "david.jpg" },
+		{ name: "Emily", position: "Vice President", pic: "emily.jpg" },
+		{ name: "Emma", position: "UI Designer", pic: "emma.jpg" },
+		{ name: "Julia", position: "Front-end Engineer", pic: "julia.jpg" },
+		{ name: "Michael", position: "Back-end Engineer", pic: "michael.jpg" },
+		{ name: "Peter", position: "Project Manager", pic: "peter.jpg" }
+	]);
 
 	return (
 		<Layout title={"Members"}>
-			<h3 className="text-8xl">{Num}</h3>
-			<button
-				className="btn"
-				onClick={() => setNum(Num + 1)}>
-				+
-			</button>
-			<button
-				className="btn"
-				onClick={() => setNum(Num - 1)}>
-				-
-			</button>
+			{Members.map(({ name, position, pic }, idx) => (
+				<article key={idx}>
+					<img src={"/" + pic} alt={name} />
+					<h3>{name}</h3>
+					<p>{position}</p>
+				</article>
+			))}
 		</Layout>
 	);
 }
