cedd5f69621fd7c7a248a9f930789e8a6e15eb38
lcy <lcy@naver.com>
2024-09-03 10:53:11 +0900

    매번 랜더링 비효율적 count 값이 바뀔때만 랜더링 되게 수정

--- src/App.jsx
+++ src/App.jsx
@@ -14,19 +14,33 @@
 	useEffect(()=>{ // 구독} return() => { // 구독해지 }),[]);
 	해당 함수가 Unmount 될 때 
 */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 	const [count, setCount] = useState(1);
+	const [name, setName] = useState("");
 
+	const handleInputChange = e => {
+		setName(e.target.value);
+	};
 	const handleCountUpdate = () => {
 		setCount(count + 1);
 	};
+
+	/* 
+	 랜더링(컴포넌트 재호출) 될때마다 매번 실행됨
+	 매번 랜더링 될때마다 실행되면 비효율적이므로 낭비가 심하므로 count 값이 바뀔때만 랜더링 되게
+	*/
+	useEffect(() => {
+		console.log("랜더링");
+	}, [count]);
 
 	return (
 		<>
 			<button onClick={handleCountUpdate}>update</button>
 			<span>count : {count}</span>
+			<input type="text" value={name} onChange={handleInputChange}></input>
+			<span>name : {name}</span>
 		</>
 	);
 }
